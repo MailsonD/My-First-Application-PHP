bd641fa67d3694336b1a9d43b5ec834153d055a9
MailsonD <mailssondennis@gmail.com>
2019-02-18 09:40:26 -0300

    Criando as primeiras páginas. Cadastro e login

---
+++ cadastro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Iniciação</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+	<body style=" background-color: gray">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="col-md-6" style="left: 330px; top: 100px">
+						<div class="jumbotron" >
+							<form>
+								<center>
+									<p>Crie seu personagem</p>
+								</center>
+								<div class="form-group">
+									<label>Nome</label>
+									<input type="text" name="nome" class="form-control">
+								</div>			
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fire Link Shrine</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/theme.css">
+</head>
+	<body class="home">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="col-md-6" style="left: 330px; top: 170px">
+						<div class="jumbotron">
+							<div>
+								<center>
+									<p style="font-style: italic;">Seja Bem vindo a nosso mundo</p>
+								</center>	
+							</div>
+							<p></p>
+							<form>
+								
+								<div class="form-group">
+									<label>Nome de guerra</label>
+									<input type="text" name="login" class="form-control">
+								</div>
+								<div class="form-group">
+									<label>Palavra mágica</label>
+									<input type="password" name="senha" class="form-control">
+								</div>
+								<div class="form-group">
+									<button type="submit" class="btn btn-success btn-block">
+										Teletransportar
+									</button>
+									<a class="btn btn-primary btn-block" href="cadastro.php">
+										Iniciação
+									</a>
+								</div>
+							</form>
+							<p></p>
+						</div> 
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
