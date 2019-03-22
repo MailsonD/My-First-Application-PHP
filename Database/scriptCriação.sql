6806416fdc927d83937aa01d0c4be745e728028f
MailsonD <mailssondennis@gmail.com>
2019-03-22 20:54:24 -0300

    Adicionando algumas funcionalidades aos scripts do banco

--- Database/scriptCriação.sql
+++ Database/scriptCriação.sql
@@ -4,4 +4,69 @@ CREATE TABLE personagem(
 	nome TEXT,
 	raca TEXT,
 	classe TEXT
+);
+
+
+CREATE TABLE nivelPersonagem(
+	nomePersonagem TEXT PRIMARY KEY,
+	nivel INT,
+	FOREIGN KEY (nomePersonagem) REFERENCES personagem(nome_de_guerra)
+);
+
+CREATE TABLE atributosPersonagem(
+	nomePersonagem TEXT PRIMARY KEY,
+	forca INT,
+	destreza INT,
+	inteligencia INT,
+	carisma INT,
+	FOREIGN KEY (nomePersonagem) REFERENCES personagem(nome_de_guerra)
 )
+
+
+
+CREATE OR REPLACE FUNCTION subirNivel(nomePers TEXT) RETURNS void
+AS $$
+	BEGIN
+		UPDATE nivelPersonagem SET nivel=nivel+1 WHERE nomePersonagem = nomePers;	
+	END
+$$LANGUAGE PLPGSQL
+
+CREATE OR REPLACE FUNCTION criaNivel() RETURNS TRIGGER
+AS $$
+	BEGIN
+		INSERT INTO nivelPersonagem VALUES(NEW.nome_de_guerra,1);
+		RETURN NULL;	
+	END
+$$LANGUAGE PLPGSQL
+
+CREATE OR REPLACE FUNCTION criaAtributos() RETURNS TRIGGER
+AS $$
+	BEGIN
+		INSERT INTO atributosPersonagem VALUES(NEW.nome_de_guerra,2,2,2,2);
+		RETURN NULL;	
+	END
+$$LANGUAGE PLPGSQL
+
+CREATE OR REPLACE FUNCTION incrementaAtributos() RETURNS TRIGGER
+AS $$
+	BEGIN
+		UPDATE atributosPersonagem SET forca = forca +1, destreza = destreza+1, inteligencia = inteligencia+1, carisma = carisma+1 WHERE nomePersonagem = NEW.nomePersonagem;
+		RETURN NULL;	
+	END
+$$LANGUAGE PLPGSQL
+
+CREATE TRIGGER adicionaNivel
+AFTER INSERT ON personagem 
+FOR EACH ROW 
+	EXECUTE PROCEDURE criaNivel();
+
+CREATE TRIGGER adicionaAtributos
+AFTER INSERT ON nivelPersonagem
+FOR EACH ROW 
+	EXECUTE PROCEDURE criaAtributos();
+
+CREATE TRIGGER incrementaAtributos
+AFTER UPDATE ON nivelPersonagem
+FOR EACH ROW 
+	EXECUTE PROCEDURE incrementaAtributos();	
+
