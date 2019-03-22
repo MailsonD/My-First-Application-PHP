677ff88c6137c3fafe88690b73c072da288bd7fa
MailsonD <mailssondennis@gmail.com>
2019-03-22 11:44:55 -0300

    Adicionando conteúdo principal a página home

--- cadastro.php
+++ cadastro.php
@@ -2,59 +2,60 @@
 <html>
 <head>
 	<title>Iniciação</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<?php
-		session_start();
+	session_start();
 
-		if(isset($_SESSION['login'])){
-			header('location:home/home.php');
-		}
+	if(isset($_SESSION['login'])){
+		header('location:home/home.php');
+	}
 	?> 
 </head>
-	<body style=" background-color: gray">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="col-md-6" style="left: 330px; top: 100px">
-						<div class="jumbotron" >
-							<form method="POST" action="Controllers/cadastro.php"/>
-								<center>
-									<p>Crie seu personagem</p>
-								</center>
-								<div class="form-group">
-									<label>Nome de guerra</label>
-									<input type="text" name="login" class="form-control">
-								</div>		
-								<div class="form-group">
-									<label>Palavra Mágina</label>
-									<input type="password" name="senha" class="form-control">
-								</div>
-								<div class="form-group">
-									<label>Nome</label>
-									<input type="text" name="nome" class="form-control">
-								</div>
-								<div class="form-group">
-									<label>Raça</label>
-									<input type="text" name="raca" class="form-control">
-								</div>	
-								<div class="form-group">
-									<label>Classe</label>
-									<input type="text" name="classe" class="form-control">
-								</div>
-							
-								<div class="form-group">
-									<button class="btn btn-success btn-block" type="submit">
-										Criar
-									</button>
-								</div>					
-							</form>
+<body style=" background-color: gray">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="col-md-6" style="left: 330px; top: 100px">
+					<div class="jumbotron" >
+						<form method="POST" action="Controllers/cadastro.php"/>
+						<center>
+							<p>Crie seu personagem</p>
+						</center>
+						<div class="form-group">
+							<label>Nome de guerra</label>
+							<input type="text" name="login" class="form-control">
+						</div>		
+						<div class="form-group">
+							<label>Palavra Mágina</label>
+							<input type="password" name="senha" class="form-control">
 						</div>
-					</div>
+						<div class="form-group">
+							<label>Nome</label>
+							<input type="text" name="nome" class="form-control">
+						</div>
+						<div class="form-group">
+							<label>Raça</label>
+							<input type="text" name="raca" class="form-control">
+						</div>	
+						<div class="form-group">
+							<label>Classe</label>
+							<input type="text" name="classe" class="form-control">
+						</div>
+						
+						<div class="form-group">
+							<button class="btn btn-success btn-block" type="submit">
+								Criar
+							</button>
+						</div>					
+					</form>
 				</div>
 			</div>
 		</div>
-	</body>
-	<?php 
-		include('../menssage.php');
-	?>
+	</div>
+</div>
+</body>
+<?php 
+include('../menssage.php');
+?>
 </html>
--- home/home.php
+++ home/home.php
@@ -2,17 +2,49 @@
 <html>
 <head>
 	<title>home</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="../css/theme.css">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	
 	<?php
 	include("../Controllers/sessionController.php");
 	?> 
 </head>
-<body>
-	logado
-	<form method="post" action="../Controllers/logout.php">
-		<button type="submit" ">Sair</button>
-	</form>
+<body class="home">
 	
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+			<a class="navbar-brand" href="home.php">Home</a>
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Edição de ficha <span class="sr-only">(current)</span></a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0" method="post" action="../Controllers/logout.php">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sair</button>
+			</form>
+		</div>
+	</nav>
+
+	<div class="row" style="margin-top:10%"></div>
+	<div class="row">
+		<div class="col-2"></div>
+		<div class="col-8">
+			<div class="jumbotron">
+				<h1 class="display-4">Hello, world!</h1>
+				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+				<hr class="my-4">
+				<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+				<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+			</div>
+		</div>
+		<div class="col-2"></div>
+	</div>
+	<div class="row" style="margin-top:10%"></div>
 </body>
 <?php 
 include('../menssage.php');
