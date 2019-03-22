10cb6dff125ac5da515c263ce1157f6802136cc1
MailsonD <mailssondennis@gmail.com>
2019-03-22 10:22:47 -0300

    Adicionando funcionalidade de logout a página principal

--- home/home.php
+++ home/home.php
@@ -2,11 +2,19 @@
 <html>
 <head>
 	<title>home</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<?php
-		include("Controllers/sessionController.php");
-	?>
+	include("../Controllers/sessionController.php");
+	?> 
 </head>
-	<body>
-		logado
-	</body>
+<body>
+	logado
+	<form method="post" action="../Controllers/logout.php">
+		<button type="submit" ">Sair</button>
+	</form>
+	
+</body>
+<?php 
+include('../menssage.php');
+?>
 </html>
